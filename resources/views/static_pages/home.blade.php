b04debc046a9e26338bdfa27de2ac14d9b6c15c1
yongfu <yongfu_sprint@163.com>
2021-02-27 15:17:18 +0800

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.default')
 @section('content')
-  <h1>首页</h1>
+  <div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">Laravel 示例</p>
+    <p>一切 已经开始</p>
+    <p><a class="btn btn-lg btn-success" href="#" role="button">注册</a></p>
+  </div>
 @stop
