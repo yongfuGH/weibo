a341105c6cdf4ae4f2aa3b4db2b8e5c889f63f4d
yongfu <yongfu_sprint@163.com>
2021-03-01 00:21:01 +0800

    登陆成功 导航添加

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
@@ -38,6 +39,7 @@ class UsersController extends Controller
             'password' => bcrypt($request->password),
         ]);
 
+        Auth::login($user);
         session()->flash('success', '欢迎，你将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
     }
