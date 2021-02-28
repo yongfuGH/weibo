bc4a8982e22153740403513469becb5a9555f7a9
yongfu <yongfu_sprint@163.com>
2021-02-28 17:06:42 +0800

    增加CSRF验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -18,5 +18,20 @@ class UsersController extends Controller
 //        return view('users.show', ['user' => $user]);
     }
 
+    /**
+     * 注册
+     * @param Request $request
+     * @return null
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+           'name' => 'required|unique:users|max:50',
+           'email' => 'required|email|unique:users|max:255',
+           'password' => 'required|confirmed|max:6'
+        ]);
+        return null;
+    }
 
 }
