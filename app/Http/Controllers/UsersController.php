a281ae0e8e040d0da01a93a9925134d3b1a5efc8
yongfu <yongfu_sprint@163.com>
2021-02-28 21:08:00 +0800

    添加用户注册

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -29,9 +29,17 @@ class UsersController extends Controller
         $this->validate($request,[
            'name' => 'required|unique:users|max:50',
            'email' => 'required|email|unique:users|max:255',
-           'password' => 'required|confirmed|max:6'
+           'password' => 'required|confirmed|max:12'
         ]);
-        return null;
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        session()->flash('success', '欢迎，你将在这里开启一段新的旅程~');
+        return redirect()->route('users.show', [$user]);
     }
 
 }
