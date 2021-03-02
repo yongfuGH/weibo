2856079b24a22357b27f3a4cef07ba80f5abc9fd
yongfu <yongfu_sprint@163.com>
2021-03-02 11:51:32 +0800

    发送找回密码链接

---
+++ app/Http/Controllers/PasswordController.php
@@ -0,0 +1,54 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
+
+
+class PasswordController extends Controller
+{
+    public function showLinkRequestForm()
+    {
+        return view('auth.passwords.email');
+    }
+
+    public function sendResetLinkEmail(Request $request)
+    {
+        // 验证邮箱
+        $request->validate(['email' => 'required|email']);
+        $email = $request->email;
+
+        // 获取用户
+        $user = User::where('email', $email)->first();
+//        若用户不存在
+        if (is_null($user)) {
+            session()->flash('danger', '邮箱未注册');
+            return redirect()->back()->withInput();
+        }
+//        生成 Token，会在视图 emails.reset_link 里拼接链接
+        $token = hash_hmac('sha256', Str::random(40), config('app.key'));
+//        入库 使用 updateOrInsert 来保持email的唯一性
+        DB::table('password_resets')->updateOrInsert(['email' => $email], [
+            'email' => $email,
+            'token' => Hash::make($token),
+            'created_at' => new Carbon,
+        ]);
+//        将 Token 链接发送给用户
+        Mail::send('emails.reset_link', compact('token'), function ($message) use ($email) {
+            $message->to($email)->subject('忘记密码');
+        });
+
+        session()->flash('success', '重置邮件发送成功，请查收');
+
+        return redirect()->back();
+    }
+
+}
